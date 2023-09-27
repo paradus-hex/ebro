f53e4d8bc8dd3f40dc709ac5b4bd488177e095bd
abrarabedin001 <abrarabedin001@gmail.com>
2023-09-27 23:16:49 +0600

    added on click outside

--- renderer/components/searchRecommendationBox.tsx
+++ renderer/components/searchRecommendationBox.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import SearchListElement from './searchListElement';
 
-export default function SearchRecommendationBox() {
+export default function SearchRecommendationBox({
+  searchRef,
+}: {
+  searchRef: React.MutableRefObject<any>;
+}) {
   return (
-    <ul className="rounded-md shadow-md bg-white absolute left-0 right-0 -bottom-18 mt-3 p-3 transition ease-in-out delay-150 ">
+    <ul
+      ref={searchRef}
+      className="rounded-md shadow-md bg-white absolute left-0 right-0 -bottom-18 mt-3 p-3 transition ease-in-out delay-150 "
+    >
       <SearchListElement name="Energy Elixir" address="project address" />
       <SearchListElement name="Energy Elixir" address="project address" />
       <SearchListElement name="Energy Elixir" address="project address" />
