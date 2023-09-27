11b79a60886047752ae22fa02816a3ab319d84e4
SabbirBhuiyan <sabbir0806@gmail.com>
2023-09-27 18:48:23 +0600

    create button and basic create page

---
+++ renderer/pages/create.tsx
@@ -0,0 +1,7 @@
+export default function Create() {
+  return (
+    <div className="w-full flex justify-center">
+      <div className="max-w-[80%]">Helo</div>
+    </div>
+  );
+}
--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -3,8 +3,14 @@ import Head from 'next/head';
 import MyProjectSlider from '../components/myProjectSlider';
 import ImageUpload from '../components/imageUpload';
 import MyProjectSlider2 from '../components/myProjectSlider2';
+import { Button } from '../components/ui/button';
+import { useRouter } from 'next/router';
 
 function Home() {
+  const router = useRouter();
+  const handleCreateClick = () => {
+    router.push('/create');
+  };
   return (
     <React.Fragment>
       <Head>
@@ -16,6 +22,9 @@ function Home() {
           {/* <MyProjectSlider2 /> */}
         </div>
       </div>
+      <Button className="w-[90px] h-[50px]" onClick={handleCreateClick}>
+        Create
+      </Button>
       <div className="w-full flex justify-center">
         <div className="max-w-[80%]">
           <ImageUpload />
