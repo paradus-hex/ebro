cb861b4d76e99d47904efec8e954fdb2fef1fe86
abrarabedin001 <abrarabedin001@gmail.com>
2023-09-27 17:24:48 +0600

    search area

--- renderer/components/myProjectSlider.tsx
+++ renderer/components/myProjectSlider.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import MyProjectCard from './myProjectCard';
+import Search from './search';
 
 export default function MyProjectSlider() {
   return (
     <div className="pt-9">
+      <Search />
       <div>
         <h1 className="text-left py-5 font-semibold">My Projects</h1>
       </div>
