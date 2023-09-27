59b5a0d489f5bdaa976b3a32b2f5bc7a941c102c
abrarabedin001 <abrarabedin001@gmail.com>
2023-09-27 15:12:27 +0600

    fixing somethings up

--- renderer/components/ui/myProjectSlider.tsx
+++ renderer/components/ui/myProjectSlider.tsx
@@ -3,10 +3,19 @@ import MyProjectCard from './myProjectCard';
 
 export default function MyProjectSlider() {
   return (
-    <div className="w-full flex flex-row overflow-x-scroll bg-black">
-      <MyProjectCard />
-      <MyProjectCard />
-      <MyProjectCard />
+    <div className="pt-9">
+      <div>
+        <h1 className="text-left">My Projects</h1>
+      </div>
+
+      <div
+        className="w-full flex flex-row overflow-x-scroll bg-black"
+        id="journal-scroll"
+      >
+        <MyProjectCard />
+        <MyProjectCard />
+        <MyProjectCard />
+      </div>
     </div>
   );
 }
