23301d20cd55856321a7b61ade8319e65cda30d6
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-09-28 21:24:44 +0600

    modal editing

--- renderer/components/finalpageModal.tsx
+++ renderer/components/finalpageModal.tsx
@@ -1,5 +1,6 @@
 // EditModal.tsx
 
+import { useRef, useEffect } from 'react';
 import React from 'react';
 
 interface EditModalProps {
@@ -23,12 +24,30 @@ const EditModal: React.FC<EditModalProps> = ({
   handleCloseModal,
   handleEditClick,
 }) => {
+  const modalRef = useRef(null);
+  const handleClickOutsideModal = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      handleCloseModal();
+    }
+  };
+  useEffect(() => {
+    if (modalRef.current) {
+      document.addEventListener('mousedown', handleClickOutsideModal);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutsideModal);
+    };
+  }, []);
   return (
-    <div className="modal">
-      <div className="modal-content">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-slate-500/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0">
+      <div
+        className="bg-white p-8 rounded-2xl shadow-md flex flex-col items-center border border-slate-700 w-5/6 h-4/6"
+        ref={modalRef}
+      >
         <h2>Edit Content</h2>
         {isEditing ? (
-          <div>
+          <div className="w-full">
             <textarea
               className="w-full flex rounded-lg h-[200px] mt-2 p-3 text-sm text-black outline-1 border border-1 border-gray-400"
               value={text}
@@ -39,7 +58,7 @@ const EditModal: React.FC<EditModalProps> = ({
             <button onClick={handleSaveClick}>Save</button>
           </div>
         ) : (
-          <div className="flex flex-col">
+          <div className="flex flex-col w-full">
             <div className="rounded-lg h-[200px] mt-2 p-3 text-sm text-black outline-0 bg-white/50 overflow-auto text-left">
               <p>{text}</p>
             </div>
