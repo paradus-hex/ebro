ea71828aa825af607112e0b8c1c5f49563387459
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-09-29 10:45:09 +0600

    search shadow

--- renderer/components/search.tsx
+++ renderer/components/search.tsx
@@ -61,7 +61,7 @@ export default function Search() {
             onChange={(e) => check(e)}
             type="text"
             placeholder="Search for elixir..."
-            className="pl-16 pr-4 py-4 rounded-md shadow bg-white border-0 w-full outline-none"
+            className="pl-16 pr-4 py-4 rounded-xl bg-white border-0 w-full outline-none shadow-md shadow-gray-300"
           />
         </div>
 
