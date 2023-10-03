477d1357dffee638a3a4ad80d1e32d0f85748cc5
abrarabedin001 <abrarabedin001@gmail.com>
2023-10-03 19:46:51 +0600

    fixing sign in and sign up

--- renderer/pages/signin.tsx
+++ renderer/pages/signin.tsx
@@ -16,12 +16,17 @@ import { useRouter } from 'next/router';
 import { Button } from '../components/ui/button';
 import { Input } from '../components/ui/input';
 import { useSignInPageStore } from '../stores/signInPageStore';
+// import { set } from 'lodash';
 
 export const signInformSchema = z.object({
   email: z.string().email({ message: 'A valid email must be provided' }),
   password: z
     .string()
     .min(8, { message: 'The password must be at least 8 characters long' }),
+  repeatPassword: z
+    .string()
+    .min(8, { message: 'The password must be at least 8 characters long' })
+    .optional(),
 });
 
 export default function signin() {
@@ -32,6 +37,8 @@ export default function signin() {
   const [showPassLogin, setShowPassLogin] = useState<boolean>(false);
   const [showPassSignUp, setShowPassSignUp] = useState<boolean>(false);
   const [showPassSignUp2, setShowPassSignUp2] = useState<boolean>(false);
+  const [repeatPass, setRepeatPass] = useState<string>('');
+  const [repeatPassError, setRepeatPassError] = useState<boolean>(false);
   const form = useForm<z.infer<typeof signInformSchema>>({
     resolver: zodResolver(signInformSchema),
     defaultValues: getStoredValues(),
@@ -51,14 +58,16 @@ export default function signin() {
               console.log('eita koj kore');
             })
             .catch((err) => console.log(err))
-        : newUser(values.email, values.password)
+        : values.password == values.repeatPassword
+        ? newUser(values.email, values.password)
             .then((e) => {
               setSignedIn(true);
               router.push('/home');
 
               console.log('eita koj kore 2');
             })
-            .catch((err) => console.log(err));
+            .catch((err) => console.log(err))
+        : setRepeatPassError(true);
     }
 
     // console.log(setProjects(values));
@@ -79,7 +88,7 @@ export default function signin() {
         <h1 className="text-4xl font-semibold mb-9 w-full">Login</h1>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <div className="grid grid-cols-1 gap-4 gap-y-14 place-items-left">
+            <div className="grid grid-cols-1 gap-4 gap-y-5 place-items-left">
               <FormField
                 control={form.control}
                 name="email"
@@ -163,7 +172,7 @@ export default function signin() {
         <h1 className="text-4xl font-semibold mb-9">Sign up</h1>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <div className="grid grid-cols-1 gap-4 gap-y-14 place-items-left">
+            <div className="grid grid-cols-1 gap-4 gap-y-5 place-items-left">
               <FormField
                 control={form.control}
                 name="email"
@@ -216,7 +225,7 @@ export default function signin() {
               />
               <FormField
                 control={form.control}
-                name="password"
+                name="repeatPassword"
                 render={({ field }) => (
                   <FormItem className="w-[80%] flex flex-col items-left">
                     <FormLabel className="text-xl">Repeat Password</FormLabel>
@@ -242,9 +251,46 @@ export default function signin() {
                       </div>
                     </FormControl>
                     <FormMessage />
+                    {repeatPassError && (
+                      <p className="text-sm p-3 text-red-700">
+                        {' '}
+                        The repeat password must match the initial password
+                      </p>
+                    )}
                   </FormItem>
                 )}
               />
+
+              {/* <div className="w-[80%] flex flex-col items-left">
+                <p className="text-xl">Repeat Password</p>
+                <div className="flex flex-col">
+                  <Input
+                    className="max-w-[920px] bg-white rounded-xl border focus:border-1 focus:border-slate-400"
+                    type={showPassSignUp2 ? 'text' : 'password'}
+                    placeholder="Password"
+                  />
+                  <div className="flex flex-row">
+                    <input
+                      className="w-[30px] mt-5"
+                      onChange={(e) => {
+                        setRepeatPass(e.target.value);
+                      }}
+                      onClick={(e) => {
+                        setShowPassSignUp2((prev) => !prev);
+                      }}
+                      type="checkbox"
+                    />
+
+                    <p className="mt-5"> Show Password</p>
+                    {repeatPassError && (
+                      <p className="text-sm p-3 text-red-700">
+                        {' '}
+                        The repeat password must match the initial password
+                      </p>
+                    )}
+                  </div>
+                </div>
+              </div> */}
             </div>
             <Button type="submit">Submit</Button>
             {/* <Button type="submit">Submit</Button> */}
