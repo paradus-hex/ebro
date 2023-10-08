c40bc91797f0f3ad91bc1975e9acaf0dd5936fb4
SabbirBhuiyan <sabbir0806@gmail.com>
2023-10-08 21:54:22 +0600

    first commit

--- renderer/pages/create.tsx
+++ renderer/pages/create.tsx
@@ -192,7 +192,27 @@ function Create() {
       isFavorite: false,
     });
     // append({ role: 'user', content: JSON.stringify(values) });
-    setChatGptRes(await chat(JSON.stringify(values)));
+    // setChatGptRes(await chat(JSON.stringify(values)));
+    fetch('https://defiant-ruby-leg-warmers.cyclic.app/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      mode: 'no-cors',
+      body: JSON.stringify(values),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('API response:', data.message);
+      })
+      .catch((error) => {
+        console.error('API error:', error);
+      });
     setStoredNote(note);
     // setLoading(isLoading);
   }
